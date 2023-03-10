2e82f2e5b0b92951ea177d597f47abaa04b35153
heeyeon-lim <heeyeon9916@gmail.com>
2023-03-10 11:36:31 +0900

    [FE][FIX] main height 조정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import GlobalStyles from './GloablStyles';
 import React from 'react';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
       <header className="header">Header</header>
-      <main></main>
+      <main>
+        <Login />
+      </main>
       <footer className="footer">Footer</footer>
     </div>
   );
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,15 +4,27 @@ import styled from 'styled-components';
 const Login: React.FC = () => {
   return (
     <>
-      <LoginForm />
+      <LoginMain>
+        <LoginForm />
+      </LoginMain>
     </>
   );
 };
 
 export default Login;
 
+const LoginMain = styled.div`
+  width: 100%;
+  /* min-height: 448px; */
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: yellow;
+`;
 const LoginForm = styled.div`
-  width: 382px;
-  height: 166px;
+  width: 503px;
+  height: 426px;
+  /* margin: auto; */
   background-color: black;
 `;
