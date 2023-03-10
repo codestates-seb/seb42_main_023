0897559b60f97f243bdcc7085cd74773d8fc3c9d
jeonjinwoo <hp5234@naver.com>
2023-03-10 17:15:22 +0900

    [BE] 개발환경 구성 및 공통기능 정의

---
+++ server/src/main/java/com/teamdragon/dragonmoney/DragonmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.teamdragon.dragonmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonmoneyApplication.class, args);
+	}
+
+}
