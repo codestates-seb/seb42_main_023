068aca0c9a19f8004bc789cc763fcd00abb74e6b
KyungaIM <avocadopajamas@gmail.com>
2023-03-11 01:16:55 +0900

    [FE][FEAT]더미데이터 store에 저장

--- client/src/store/store.tsx
+++ client/src/store/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import slice from '../slices/slice';
+import postSlice from '../slices/postSlice';
 
 const store = configureStore({
   reducer: {
     slice: slice.reducer,
+    postlist: postSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
