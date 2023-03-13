b14fa7dff74aa0276a105ace5c7ebe1f8bc45abb
Minhyung Lee <freeman99000@gmail.com>
2023-03-13 15:25:23 +0900

    [FE][FEAT] CreatePost 컴포넌트 및 태그 기능 구현

--- client/src/components/createPostP/TitleInput.tsx
+++ client/src/components/createPostP/TitleInput.tsx
@@ -1,5 +1,43 @@
-function TitleInput() {
-  return <div></div>;
-}
+import React from 'react';
+import styled from 'styled-components';
+import { useAppDispatch } from '../../hooks';
+import { setTitle } from '../../slices/postInputSlice';
+
+const TitleInput: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const valueCheck = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    dispatch(setTitle(event.target.value));
+  };
+
+  return (
+    <TitleInputContainer>
+      <h1> 게시글 작성</h1>
+      <Input
+        className="title-input"
+        placeholder="제목을 입력해주세요."
+        onChange={valueCheck}
+      ></Input>
+    </TitleInputContainer>
+  );
+};
 
 export default TitleInput;
+
+const TitleInputContainer = styled.div`
+  width: 100%;
+  height: 100px;
+  margin-top: 50px;
+  margin-bottom: 20px;
+  h1 {
+    font-size: 24px;
+    font-weight: 400;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 50px;
+  border: 1px solid #d4d4d4;
+  padding: 0 10px 0 10px;
+  margin-top: 20px;
+`;
