78b9c52e74efe9f7563732d50c851fcec780745d
Minhyung Lee <freeman99000@gmail.com>
2023-03-13 15:50:01 +0900

    [FE][FEAT] PostDetail 인기글 데이터 패칭 구현

--- client/src/components/postDetailP/ProfilePreview.tsx
+++ client/src/components/postDetailP/ProfilePreview.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ProfilePreview = () => {
+const ProfilePreview: React.FC = () => {
   return (
     <ProfilePreviewContainer>
       <ConfirmUserInfoBtn>더 보기</ConfirmUserInfoBtn>
--- client/src/sagas/post/postSaga.ts
+++ client/src/sagas/post/postSaga.ts
@@ -1,8 +1,34 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 import { postSagaActions } from './postSagaActions';
-//
-console.log(postSagaActions);
+interface Response {
+  config: object;
+  data: Array<object>;
+  headers: object;
+  request: object;
+  status: number;
+  statusText: string;
+}
+
+const getPost = async () => {
+  const response = await axios.get(
+    'https://main-project-d9049-default-rtdb.asia-southeast1.firebasedatabase.app/BEST_AWARDS_POST.json',
+  );
+  return response.data;
+};
+
+function* fetchPost(action: any) {
+  try {
+    const result: Response = yield call(getPost);
+    console.log('result', result);
+    yield put({
+      type: postSagaActions.SUCCESS_GET_POPULAR_POST,
+      payload: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 function* handleToggle(action: any) {
   // 미들웨어 동작 로직
@@ -16,6 +42,8 @@ function* postSaga() {
   yield takeLatest(postSagaActions.TOGGLE_COMMENT_DISLIKE, handleToggle);
   yield takeLatest(postSagaActions.TOGGLE_REPLY_LIKE, handleToggle);
   yield takeLatest(postSagaActions.TOGGLE_REPLY_DISLIKE, handleToggle);
+  yield takeLatest(postSagaActions.TOGGLE_REPLY_DISLIKE, handleToggle);
+  yield takeLatest(postSagaActions.GET_POPULAR_POST, fetchPost);
 }
 
 export default postSaga;
