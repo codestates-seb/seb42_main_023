9d978b8b9a55bac0f7f04ebcfa68c5455e45f51a
heeyeon-lim <heeyeon9916@gmail.com>
2023-03-13 23:38:23 +0900

    [FE][STYLE] recommendloan 페이지 디자인 구현중

--- client/src/components/recommendLoan/Intro.tsx
+++ client/src/components/recommendLoan/Intro.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 import BlueBtn from '../common/BlueBtn';
+import { useState } from 'react';
+import Q1 from './Q1';
 
 const Intro = () => {
+  const [resultCode, setResultCode] = useState<null | number>(0);
+
   return (
     <IntroContainer>
       <div>Logo</div>
-      <IntroContent>
-        <h2>
-          <strong>1~2%대</strong> 금리로 제공받을 수 있는 <br />
-          <strong>공공전월세 대출</strong>이 있다는 것 알고 계셨나요?
-        </h2>
-        <p>간단한 설문을 통해 나에게 맞는 공공대출 상품을 찾아보세요</p>
-      </IntroContent>
-      <BlueBtn
-        width="300px"
-        height="50px"
-        content="나에게 맞는 대출상품 찾기"
-        onClick={() => console.log('clicked!hi!')}
-      />
+      {resultCode === 0 ? (
+        <>
+          <Question>
+            <h2>
+              <strong>1~2%대</strong> 금리로 제공받을 수 있는 <br />
+              <strong>공공전월세 대출</strong>이 있다는 것 알고 계셨나요?
+            </h2>
+            <p>간단한 설문을 통해 나에게 맞는 공공대출 상품을 찾아보세요</p>
+          </Question>
+          <Answer>
+            <BlueBtn
+              width="300px"
+              height="50px"
+              content="나에게 맞는 대출상품 찾기"
+              onClick={() => setResultCode(1)}
+            />
+          </Answer>
+        </>
+      ) : (
+        <Q1 />
+      )}
     </IntroContainer>
   );
 };
@@ -26,19 +38,21 @@ const Intro = () => {
 export default Intro;
 
 const IntroContainer = styled.div`
-  width: 60%;
-  height: 70%;
+  width: 65%;
+  height: 75%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: center;
+  background-color: aliceblue;
 `;
 
-const IntroContent = styled.div`
+const Question = styled.div`
   width: 100%;
+  background-color: red;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin: 50px 0px;
   > h2 {
     text-align: center;
     > strong {
@@ -49,4 +63,8 @@ const IntroContent = styled.div`
     color: #5c5c5c;
     margin-top: 30px;
   }
+`;
+
+const Answer = styled.div`
+  background-color: green;
 `;
--- client/src/pages/RecommendLoan.tsx
+++ client/src/pages/RecommendLoan.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
 import NavRealEstate from '../components/common/NavRealEstate';
+import Intro from '../components/recommendLoan/Intro';
 
-function RecommendLoan() {
+const RecommendLoan: React.FC = () => {
   return (
-    <div>
+    <MainContainer>
       <NavRealEstate />
-      <p>This is recommendloan page</p>
-    </div>
+      <div>
+        <SurveyBox>
+          <Intro />
+        </SurveyBox>
+      </div>
+    </MainContainer>
   );
-}
+};
 
 export default RecommendLoan;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  > div {
+    width: 850px;
+    height: 80%;
+    min-height: 500px;
+    margin-left: 40px;
+    background-color: #f8f8f8;
+    border: 1px solid #d4d4d4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const SurveyBox = styled.div`
+  width: 80%;
+  height: 90%;
+  background-color: #fff;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
