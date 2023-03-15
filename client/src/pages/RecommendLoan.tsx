a0883418d3e67c6b45de4bf597bc6f166ca9aa1d
heeyeon-lim <heeyeon9916@gmail.com>
2023-03-15 14:17:44 +0900

    [FE][CHORE]빌드 준비

--- client/src/pages/RecommendLoan.tsx
+++ client/src/pages/RecommendLoan.tsx
@@ -1,6 +1,6 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import NavRealEstate from '../components/common/NavRealEstate';
+import React from 'react';
+import styled from 'styled-components';
+import NavRealEstate from '../components/common/NavRealEstate';
 // import { questionData } from '../data/surveyData';
 // import Intro from '../components/recommendLoan/Intro';
 // import Question from '../components/recommendLoan/Question';
@@ -20,58 +20,59 @@
 //     })[];
 // }
 
-// const RecommendLoan: React.FC = () => {
-//   const [currentQuestion, setCurrentQuestion] = useState(null);
-//   const nextQuestionHandler = (next: number) => {
-//     if (next === next) {
-//       setCurrentQuestion(questionData[next]);
-//     // } else if () {
-//     //   next === resultId
-//     //   move to result page.
-//     // }
-//   };
+const RecommendLoan: React.FC = () => {
+  //   const [currentQuestion, setCurrentQuestion] = useState(null);
+  const nextQuestionHandler = (next: number) => {
+    // if (next === next) {
+    //   setCurrentQuestion(questionData[next]);
+    // } else if () {
+    //   next === resultId
+    //   move to result page.
+    // }
+    // }
+  };
 
-//   return (
-//     <MainContainer>
-//       <NavRealEstate />
-//       <div className="content-container">
-//         <SurveyBox>
-//           {currentQuestion! ? (
-//             <Intro nextQuestionHandler={nextQuestionHandler} />
-//           ) : (
-//             <Question />
-//           )}
+  return (
+    <MainContainer>
+      <NavRealEstate />
+      <div className="content-container">
+        <SurveyBox>
+          {/* {currentQuestion! ? (
+            <Intro nextQuestionHandler={nextQuestionHandler} />
+          ) : (
+            <Question />
+          )} */}
 
-//           {/* <Result /> */}
-//         </SurveyBox>
-//       </div>
-//     </MainContainer>
-//   );
-// };
+          {/* <Result /> */}
+        </SurveyBox>
+      </div>
+    </MainContainer>
+  );
+};
 
-// export default RecommendLoan;
+export default RecommendLoan;
 
-// const MainContainer = styled.div`
-//   display: flex;
-//   > .content-container {
-//     border: 1px solid #d4d4d4;
-//     background-color: #f8f8f8;
-//     margin-left: 20px;
-//     width: calc(100% - 160px);
-//     height: 600px;
-//     padding: 20px;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//   }
-// `;
+const MainContainer = styled.div`
+  display: flex;
+  > .content-container {
+    border: 1px solid #d4d4d4;
+    background-color: #f8f8f8;
+    margin-left: 20px;
+    width: calc(100% - 160px);
+    height: 600px;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
-// const SurveyBox = styled.div`
-//   width: 80%;
-//   height: 95%;
-//   background-color: #fff;
-//   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-//   border-radius: 20px;
-//   display: flex;
-//   justify-content: center;
-// `;
+const SurveyBox = styled.div`
+  width: 80%;
+  height: 95%;
+  background-color: #fff;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+`;
