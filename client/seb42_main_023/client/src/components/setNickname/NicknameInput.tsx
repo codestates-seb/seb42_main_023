76ef43c812354221fbcec0471db6372490afb64e
KyungaIM <avocadopajamas@gmail.com>
2023-03-19 22:52:47 +0900

    [FE][CHORE]중복폴더 삭제

--- client/seb42_main_023/client/src/components/setNickname/NicknameInput.tsx
+++
@@ -1,6 +0,0 @@
-// 공통 컴포넌트
-function NicknameInput() {
-  return <div></div>;
-}
-
-export default NicknameInput;
--- client/src/components/mainP/Pagenation.tsx
+++ client/src/components/mainP/Pagenation.tsx
@@ -22,7 +22,7 @@ const Pagination = () => {
   //     setActivePage(number);
   //     paginate(number);
   //   };
-  const pageNumbers = [1, 2, 3, 4, 5];
+  const pageNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
     <Pages>
