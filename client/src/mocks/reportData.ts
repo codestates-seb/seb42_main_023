78411f4515673002e4338667bf1fa86ff84dd9f8
heeyeon-lim <heeyeon9916@gmail.com>
2023-03-24 23:59:10 +0900

    [FE][FEAT] msw사용하여 report dummy data 생성

---
+++ client/src/mocks/reportData.ts
@@ -0,0 +1,280 @@
+export const singleReport = {
+  reportId: 1,
+  reportedAt: '2019-11-12T16:34:30.388',
+  reportCategory: '양리목적/홍보성',
+  targetType: '댓글',
+  content: '광고. 이 보험 사세요!',
+  writer: '작성자',
+  reporter: '신고자',
+  description: '보험 상품을 판매하는 글이여서 신고했습니다.',
+  postId: 2,
+};
+
+export const reportList = {
+  pageInfo: {
+    page: 1,
+    size: 14,
+    totalElement: 123,
+    totalPage: 13,
+  },
+  reports: [
+    {
+      reportId: 1,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 2,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 3,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 4,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+    },
+    {
+      reportId: 5,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 6,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 7,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 8,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 9,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 10,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 11,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 12,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 13,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 14,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 15,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 16,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 17,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 18,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 19,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 20,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 21,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 22,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 23,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 24,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 25,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+    {
+      reportId: 26,
+      reportedAt: '2019-11-12T16:34:30.388',
+      reportCategory: '양리목적/홍보성',
+      targetType: '게시글',
+      writer: '작성자',
+      postId: 2,
+      description:
+        '광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다. 광고글입니다!!',
+    },
+  ],
+};
