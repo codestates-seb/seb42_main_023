6bbb12ad07b1d321cbe570976a6d2e359c8d1884
heeyeon-lim <heeyeon9916@gmail.com>
2023-03-25 00:04:15 +0900

    fixup! [FE][FEAT] msw사용하여 report dummy data 생성

--- client/src/mocks/worker.ts
+++ client/src/mocks/worker.ts
@@ -1,5 +1,10 @@
 import { setupWorker } from 'msw';
 import { handlers } from './handlers';
 import { memberHandlers } from './memberHandler';
+import { reportHanlders } from './reportHandlers';
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers, ...memberHandlers);
+export const worker = setupWorker(
+  ...handlers,
+  ...memberHandlers,
+  ...reportHanlders,
+);
