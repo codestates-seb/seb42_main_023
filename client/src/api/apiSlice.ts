74ccaba3bb89bc70bad7802ff1c2407b38bb47c2
heeyeon-lim <heeyeon9916@gmail.com>
2023-03-24 10:22:11 +0900

    [FE][FEAT] 권한 오류 error handling

--- client/src/api/apiSlice.ts
+++ client/src/api/apiSlice.ts
@@ -14,12 +14,31 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-// access token 이 만료되었을때 실행하는 refresh token flow
+// 권한 관련 오류와 access token 이 만료되었을때 실행하는 refresh token flow
 const baseQueryWithReauth = async (args: any, api: any, extraOptions: any) => {
   let result = await baseQuery(args, api, extraOptions);
 
-  // 만약 만료된 access token을 보내 403에러를 받으면, 새로운 access token 발급을 위해 refresh token 보내기
-  if (result?.error?.status === 403) {
+  // 접근 권한이 없는 경우 (일반 유저가 관리자 페이지로 들어갔을때)
+  if (
+    result?.error?.status === 403 &&
+    result.meta?.response?.headers.get('message') === 'User unauthorized'
+  ) {
+    alert('접근할 수 없는 페이지입니다.');
+  }
+
+  // 기타 인증과정 오류 발생 (내가 쓴 글이 아닌데 삭제나 수정할때)
+  if (
+    result?.error?.status === 403 &&
+    result.meta?.response?.headers.get('message') === 'Authorized Fail'
+  ) {
+    alert('요청을 수행할 수 없습니다.');
+  }
+
+  // access token이 만료되었다는 status와 메세지를 받으면, 새로운 access token 발급을 위해 refresh token 보내기
+  if (
+    result?.error?.status === 401 &&
+    result.meta?.response?.headers.get('message') === 'Access token expired'
+  ) {
     const name = localStorage.getItem('name');
     const refreshResult = await baseQuery(
       `/auth/refresh/${name}`,
