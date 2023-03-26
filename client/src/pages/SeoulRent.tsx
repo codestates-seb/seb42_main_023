2352f5456785d5042f432c32760597098399eed7
KyungaIM <avocadopajamas@gmail.com>
2023-03-26 16:37:23 +0900

    [FE][FIX]잘못 받은 모듈 위치 수정

--- client/src/pages/SeoulRent.tsx
+++ client/src/pages/SeoulRent.tsx
@@ -3,55 +3,55 @@ import styled from 'styled-components';
 import { MainContainer } from './RecommendLoan';
 import NavRealEstate from '../components/common/NavRealEstate';
 import Seoulmap from '../assets/Seoulmap.png';
-// import './style.css';
-// import { MapAreas, Map } from 'react-img-mapper/dist/types';
-
-// export interface Maptype {
-//   id?: string;
-//   shape: string;
-//   coords: []; // <== Problem from react-img-mapper
-//   active: boolean;
-//   disabled: boolean;
-//   href: string;
-//   fillColor: string;
-//   strokeColor: string;
-//   lineWidth: number;
-//   preFillColor: string;
-// }
-// type Map = {
-//   name: string;
-//   areas: Array<MapAreas>;
-// }
+import { Map, MapAreas } from 'react-img-mapper/dist/types';
+import ImageMapper from 'react-img-mapper/dist/types';
 
 function SeoulRent() {
-  // const AREAS_MAP: MapAreas = {
-  //   name: 'mymap',
-  //   areas: [
-  //     {
-  //       id: 'two',
-  //       shape: 'circle',
-  //       coords: [185, 680, 117],
-  //       preFillColor: 'red',
-  //       fillColor: 'blue',
-  //       active: false,
-  //       strokeColor: 'red',
-  //       disabled: false,
-  //       href: 'href',
-  //       lineWidth: 1,
-  //     },
-  //   ],
-  // };
+  const URL = 'https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg';
+  const AREAS_MAP: Map = {
+    name: 'my-map',
+    areas: [
+      {
+        id: '1',
+        shape: 'poly',
+        coords: [25, 33, 27, 300, 128, 240, 128, 94],
+        preFillColor: 'green',
+        fillColor: 'blue',
+      },
+      {
+        id: '2',
+        shape: 'poly',
+        coords: [219, 118, 220, 210, 283, 210, 284, 119],
+        preFillColor: 'pink',
+      },
+      {
+        id: '3',
+        shape: 'poly',
+        coords: [381, 241, 383, 94, 462, 53, 457, 282],
+        fillColor: 'yellow',
+      },
+      {
+        id: '4',
+        shape: 'poly',
+        coords: [245, 285, 290, 285, 274, 239, 249, 238],
+        preFillColor: 'red',
+      },
+      { id: '5', shape: 'circle', coords: [170, 100, 25] },
+    ],
+  };
   return (
-    <div>
-      서울
-      {/* <ImageMapper src={img} map={AREAS_MAP} /> */}
-    </div>
+    <MainContent>
+      <NavRealEstate />
+      <div className="content-container">
+        {/* <ImageMapper src={URL} map={AREAS_MAP} /> */}
+      </div>
+    </MainContent>
   );
 }
 
 export default SeoulRent;
-// const MainContent = styled(MainContainer)`
-//   .content-container {
-//     background-color: #fff;
-//   }
-// `;
+const MainContent = styled(MainContainer)`
+  .content-container {
+    background-color: #fff;
+  }
+`;
