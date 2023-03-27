768cd106465038520108bb7b0fd1aa974bb7566e
KyungaIM <avocadopajamas@gmail.com>
2023-03-28 08:04:59 +0900

    [FE][FEAT]api test완료

--- client/src/pages/SeoulRent.tsx
+++ client/src/pages/SeoulRent.tsx
@@ -8,41 +8,41 @@ import ImageMapper from 'react-img-mapper';
 import { CustomArea, AreaEvent } from 'react-img-mapper/dist/types';
 import { seoulrentApi } from '../api/seoulrentApi';
 import { Link } from 'react-router-dom';
+
 interface StyleProps {
   left: number;
   top: number;
 }
 function SeoulRent() {
+  // 데이터 api
   const seoulrentquery = seoulrentApi.useGetSeoulRentListQuery({
     query: '',
   });
-  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
   const { data, isSuccess } = seoulrentquery;
-  // const [x, setX] = useState(0);
-  // const [y, setY] = useState(0);
-  const [clickedArea, setClickedArea] = useState('');
-  const [msg, setMsg] = useState('');
 
-  // useEffect(() => {
-  //   console.log(x, y, clickedArea + 'text');
-  // }, [x, y]);
+  //툴팁 위치정보 저장
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+  //클릭한 지역정보
+  const [clickedArea, setClickedArea] = useState('');
+  //클릭한 지역 월세
+  const [monthlyRent, setMonthlyRent] = useState('');
 
   const moveOnArea = (area: CustomArea, evt: AreaEvent) => {
     const coords = { x: evt.nativeEvent.clientX, y: evt.nativeEvent.clientY };
-    console.log(
-      `You moved on ${area.shape} ${area.id} at coords ${JSON.stringify(
-        coords,
-      )} !`,
-    );
-    setTooltipPosition(coords);
-    // setX(coords.x);
-    // setY(coords.y);
     setClickedArea(area.id as string);
+    const dataItem = data.locationList.find(
+      (el: Seoulrent) => el.location === clickedArea,
+    );
+
+    console.log(dataItem);
+    setTooltipPosition(coords);
   };
+
   return (
     <MainContent>
       <NavRealEstate />
       <div className="content-container">
+        {/* {isSuccess && data.locationList[0].location} */}
         <ImageMapper
           src={Seoulmap}
           map={AREAS_MAP}
@@ -50,7 +50,7 @@ function SeoulRent() {
           height={530}
           onMouseUp={(area, _, evt) => moveOnArea(area, evt)}
         />
-        {clickedArea && (
+        {clickedArea && isSuccess && (
           <TooltipWrap left={tooltipPosition.x} top={tooltipPosition.y}>
             <div>{clickedArea}</div>
             <div>
@@ -59,7 +59,7 @@ function SeoulRent() {
             </div>
             <div className="lastdiv">
               <span>월세 </span>
-              {}
+              {monthlyRent}
             </div>
             <div>
               <Link className="link" to="/happyhouse">
