b27da31930b2ec8ce5fb687b6bdfefa4a8080362
YeYoung0-0 <mimyoy.y@gmail.com>
2023-03-31 12:16:53 +0900

    [BE][FIX] 버그 수정 2

--- server/src/main/java/com/teamdragon/dragonmoney/app/domain/report/repository/ReportRepositoryImpl.java
+++ server/src/main/java/com/teamdragon/dragonmoney/app/domain/report/repository/ReportRepositoryImpl.java
@@ -230,8 +230,7 @@ public class ReportRepositoryImpl implements ReportRepositoryCustom{
             List<Report> content = queryFactory
                     .selectFrom(report)
                     .distinct()
-                    .where(report.handleState.eq(handledState))
-                    .where(reply.state.eq(Reply.State.REPOTED))
+                    .where(report.handleState.eq(handledState), reply.state.eq(Reply.State.REPOTED))
                     .offset(pageable.getOffset())
                     .limit(pageable.getPageSize())
                     .fetch();
