df6df41bdd3251a189e702519f239b7d8d886dff
Minhyung Lee <freeman99000@gmail.com>
2023-03-31 14:30:17 +0900

    [FE][FIX] 게시물 상세 드롭다운 창 버그 수정

--- client/src/pages/PostDetail.tsx
+++ client/src/pages/PostDetail.tsx
@@ -164,6 +164,9 @@ const PostDetail: React.FC = () => {
 
   // 페이지 이동 시 스크롤 최상단 이동
   useEffect(() => {
+    if ('post' in state && state.post.isOpenFilter) {
+      dispatch(setIsOpenFilter(true));
+    }
     scrollTo(0, 0);
     refetch();
   }, [postId]);
